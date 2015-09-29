388ac8a94d6de4dea794f479aea3fc95c1b798bd
robinharms <robin@betahaus.net>
2015-09-29 17:25:58 +0200

    Email implementation

--- arche_2fa/app/email/__init__.py
+++ arche_2fa/app/email/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.httpexceptions import HTTPForbidden
+from pyramid.renderers import render
 
 from arche_2fa.models import TwoFactAuthHandler
 from arche_2fa import _
@@ -8,15 +9,18 @@ class Email2FA(TwoFactAuthHandler):
     name = 'email'
     title = _("Email")
 
-    def send(self, userid):
+    def send(self, userid, view):
         user = self.context['users'].get(userid, None)
         if user and user.email:
             token = self.create_token(userid, length = 6)
             subject = _("2fa_email_subject",
                         default = "${head_title}: Two factor authentication code",
                         mapping = {'head_title': self.context.head_title})
-            html = "Code: %s" % str(token)
+            response = {'code': str(token), 'user': user, 'view': view}
+            html = render('arche_2fa.app.email:email.pt', response, request = self.request)
             self.request.send_email(subject, [user.email], html, send_immediately = True)
+            msg = _("Email sent")
+            view.flash_messages.add(msg, type = 'success')
         else:
             raise HTTPForbidden(_("UserID or email address invalid."))
 
